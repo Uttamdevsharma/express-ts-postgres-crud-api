448a5624806a306136ceade2def5e4cf00f89313
Uttam <uttamrohit4545@gmail.com>
2025-12-02 22:03:37 +0600

    feat : post request

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,23 @@
 import express, { Request, Response } from "express"
 const app = express()
-const port = 3000
+const port = 5000
+
+
+//middleware
+app.use(express.json())
 
 app.get('/', (req:Request, res:Response) => {
   res.send('Full Stack Developer!')
+})
+
+app.post('/',(req,res) => {
+  const result = req.body
+  console.log(result)
+
+  res.send({
+    message : "successfully",
+    result
+  })
 })
 
 app.listen(port, () => {
