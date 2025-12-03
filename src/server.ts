691fd1dfa93638c9ce4777aa2beddc917ae01f82
Uttam <uttamrohit4545@gmail.com>
2025-12-03 17:15:51 +0600

    feat : get all users

--- src/server.ts
+++ src/server.ts
@@ -79,6 +79,13 @@ app.post("/users", async(req, res) => {
 
 app.get("/users",async(req:Request,res:Response) => {
   try{
+    const result = await pool.query(`SELECT * FROM users`);
+
+    res.status(200).json({
+      success:true,
+      message:"successfully retrieved",
+      data : result.rows
+    })
 
   }catch(error:any){
     res.status(500).json({
