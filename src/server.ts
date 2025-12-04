008181eb5d5ac586e57bb38c83fcb4ab204302cf
Uttam <uttamrohit4545@gmail.com>
2025-12-04 14:25:36 +0600

    feat:configuration

--- src/server.ts
+++ src/server.ts
@@ -1,45 +1,12 @@
+import config  from "./config";
 import express, { NextFunction, Request, Response } from "express";
-import { Pool } from "pg";
+import initDB, { pool } from "./config/db";
+
 const app = express();
-const port = 5000;
-import dotenv from 'dotenv'
-import path from 'path'
+const port = config.port;
 
-dotenv.config({path:path.join(process.cwd(),".env")})
 
-const pool = new Pool({
-  connectionString: `${process.env.CONNECTION_STR}`,
-});
-
-const initDB = async () => {
-  await pool.query(`
-    CREATE TABLE IF NOT EXISTS users(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(150) UNIQUE NOT NULL,
-    age INT,
-    phone VARCHAR(15),
-    address TEXT,
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW()
-    ) 
-    `);
-    // await pool.query('DROP TABLE IF EXISTS todos CASCADE');
-    await pool.query(`
-      CREATE TABLE IF NOT EXISTS todos(
-        id SERIAL PRIMARY KEY,
-        user_id INT REFERENCES users(id) ON DELETE CASCADE,
-        title VARCHAR(200) NOT NULL,
-        description TEXT,
-        completed BOOLEAN DEFAULT false,
-        due_date DATE,
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW()
-      )
-    `)    
-
-};
-
+//initializing db
 initDB();
 
 const logger = (req:Request,res:Response,next:NextFunction) => {
@@ -238,7 +205,6 @@ app.use((req,res) => {
     path: req.path
   })
 })
-
 
 
 
