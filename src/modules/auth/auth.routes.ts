72920b42332d054e3e3866f3f98ccb20d9b46eb2
Uttam <uttamrohit4545@gmail.com>
2025-12-04 23:51:36 +0600

    feat : auth modules

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,8 +1,9 @@
 import express from 'express'
+import { authController } from './auth.controller'
 
 const router = express.Router()
 
 
-router.post("/login",)
+router.post("/login",authController.loginUser)
 
 export const authRoutes = router
---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,36 @@
+import  jwt  from 'jsonwebtoken';
+import bcrypt  from 'bcryptjs';
+import { pool } from "../../config/db"
+import config from '../../config';
+
+
+
+const loginUser = async(email:string,password:string) => {
+    const result =await pool.query(`SELECT * FROM users WHERE email=$1` , [email])
+
+    if(result.rows.length === 0){
+        return false
+    }
+
+    const user = result.rows[0]
+
+    const match =await bcrypt.compare(password,user.password)
+
+    if(!match){
+        return false
+    }
+
+    const  token = jwt.sign(
+        { name:user.name , email:user.email , role : user.role, },config.jwt_secret as string,
+        {
+            expiresIn:"7d"
+        }
+    )
+
+    return {token,user}
+
+}
+
+export const authService = {
+    loginUser
+}
